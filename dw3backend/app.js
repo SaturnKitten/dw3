f6d5d6e72d49dcf63ccef43d43d59bc723ecfa41
IzadoraMS <izadora.macedo2205@gmail.com>
2025-09-16 08:10:56 -0300

    Início da configuração do diretório apps

--- dw3backend/app.js
+++ dw3backend/app.js
@@ -2,8 +2,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 
-const app = express(); const port = 40000;
+const router = require('./routes/router');
+ 
+const app = express();
+const port = 40000;
+
+app.use(bodyParser.urlencoded({ extended: false, }));
+app.use(express.json());
+
+//@ Utiliza o routerApp configurado em ./routes/route.js
+app.use(router);
+
+
 
 app.listen(port, () => {
-	console.log(`App listening at port ${port}`)
+  console.log(`App listening at port ${port}`)
 })
